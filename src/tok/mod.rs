9badd47a79595df980930b140d3849a0e463c2ff
gwenn <gtreguier@gmail.com>
2016-05-27 21:48:19 +0200

    Add some tests

--- src/tok/mod.rs
+++ src/tok/mod.rs
@@ -463,9 +463,7 @@ impl<'input> Tokenizer<'input> {
                     self.bump();
                     Some(Ok((idx0, BitNot, idx0 + 1)))
                 }
-                Some((idx0, c)) if c == '`' || c == '\'' || c == '"' => {
-                    Some(self.literal(idx0, c))
-                }
+                Some((idx0, c)) if c == '`' || c == '\'' || c == '"' => Some(self.literal(idx0, c)),
                 Some((idx0, '.')) => {
                     match self.bump() {
                         Some((_, c)) if c.is_digit(10) => Some(self.fractional_part(idx0)),
@@ -744,7 +742,7 @@ impl<'input> Iterator for Tokenizer<'input> {
                 Some(Err(Error {
                     location: location + self.shift,
                     code: code,
-                    line : line,
+                    line: line,
                 }))
             }
         }
@@ -761,4 +759,174 @@ fn is_identifier_continue(c: char) -> bool {
     (c >= 'a' && c <= 'z') || c > '\x7F'
 }
 
-// TODO test INSERT INTO t3 VALUES( 'r c', '');
+#[cfg(test)]
+mod test {
+    use super::{Error, ErrorCode, Tokenizer, Tok};
+
+    fn assert_tokens(expected_tokens: Vec<Tok>, input: &str) {
+        let lexer = Tokenizer::new(input, 0);
+        let mut expected_tokens_it = expected_tokens.into_iter();
+        for actual in lexer {
+            assert!(actual.is_ok());
+            let (_, actual_token, _) = actual.unwrap();
+            let expected_token = expected_tokens_it.next().unwrap();
+            assert_eq!(expected_token, actual_token);
+        }
+        let missing_token = expected_tokens_it.next();
+        assert_eq!(None, missing_token);
+    }
+
+    fn assert_error(expected_tokens: Vec<Result<Tok, Error>>, input: &str) {
+        let lexer = Tokenizer::new(input, 0);
+        let mut expected_tokens_it = expected_tokens.into_iter();
+        for actual in lexer {
+            let expected = expected_tokens_it.next().unwrap();
+            match (actual, expected) {
+                (Ok((_, actual_token, _)), Ok(expected_token)) => {
+                    assert_eq!(expected_token, actual_token);
+                }
+                (Err(Error { code: actual_code, .. }), Err(Error { code: expected_code, .. })) => {
+                    assert_eq!(expected_code, actual_code);
+                }
+                (actual, expected) => panic!("expected: {:?}, got: {:?}", expected, actual),
+            }
+        }
+        let missing_token = expected_tokens_it.next();
+        assert_eq!(None, missing_token);
+    }
+
+    #[test]
+    fn test_insert() {
+        let expected_tokens = vec![Tok::Insert,
+                                   Tok::Into,
+                                   Tok::Id("t3"),
+                                   Tok::Values,
+                                   Tok::LeftParen,
+                                   Tok::StringLiteral("r c"),
+                                   Tok::Comma,
+                                   Tok::StringLiteral(""),
+                                   Tok::RightParen,
+                                   Tok::Semi];
+        assert_tokens(expected_tokens, "INSERT INTO t3 VALUES( 'r c', '');");
+        let expected_tokens = vec![Tok::Insert,
+                                   Tok::Into,
+                                   Tok::Id("email"),
+                                   Tok::LeftParen,
+                                   Tok::Id("from"),
+                                   Tok::Comma,
+                                   Tok::Id("to"),
+                                   Tok::Comma,
+                                   Tok::Id("subject"),
+                                   Tok::Comma,
+                                   Tok::Id("body"),
+                                   Tok::RightParen,
+                                   Tok::Values,
+                                   Tok::LeftParen,
+                                   Tok::StringLiteral("..."),
+                                   Tok::Comma,
+                                   Tok::StringLiteral("..."),
+                                   Tok::Comma,
+                                   Tok::StringLiteral("..."),
+                                   Tok::Comma,
+                                   Tok::StringLiteral("..."),
+                                   Tok::RightParen];
+        assert_tokens(expected_tokens,
+                      "INSERT INTO email([from],[to],subject,body) VALUES('...', '...', '...', \
+                       '...')");
+    }
+
+    #[test]
+    fn test_comment() {
+        let expected_tokens = vec![];
+        assert_tokens(expected_tokens, "-- ......");
+        let expected_tokens = vec![];
+        assert_tokens(expected_tokens, " -- ......\n");
+    }
+
+    #[test]
+    fn test_minus() {
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::Minus, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a-b");
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::Minus, Tok::Minus, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a - -b");
+    }
+
+    #[test]
+    fn test_plus() {
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::Plus, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a+b");
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::Plus, Tok::Plus, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a + +b");
+    }
+
+    #[test]
+    fn test_star() {
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::Star, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a*b");
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::Star, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a * b");
+    }
+
+    #[test]
+    fn test_block_comment() {
+        let expected_tokens = vec![Tok::Select, Tok::Id("a")];
+        assert_tokens(expected_tokens, "SELECT a/*b***/");
+    }
+
+    #[test]
+    fn test_slash() {
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::Slash, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a/b");
+    }
+
+    #[test]
+    fn test_reminder() {
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::Reminder, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a%b");
+    }
+
+    #[test]
+    fn test_equals() {
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::Equals, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a=b");
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::Equals, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a==b");
+    }
+
+    #[test]
+    fn test_less() {
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::LessThan, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a<b");
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::LessEquals, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a<=b");
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::NotEquals, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a<>b");
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::LeftShift, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a<<b");
+    }
+
+    #[test]
+    fn test_greater() {
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::GreaterThan, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a>b");
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::GreaterEquals, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a>=b");
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::RightShift, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a>>b");
+    }
+
+    #[test]
+    fn test_exclamation_mark() {
+        let expected_tokens = vec![Tok::Select, Tok::Id("a"), Tok::NotEquals, Tok::Id("b")];
+        assert_tokens(expected_tokens, "SELECT a!=b");
+    }
+
+    #[test]
+    fn test_exclamation_mark_alone() {
+        let expected_tokens = vec![Ok(Tok::Select),
+                                   Ok(Tok::Id("a")),
+                                   super::error(ErrorCode::ExpectedEqualsSign, 0, ""),
+                                   Ok(Tok::Id("b"))];
+        assert_error(expected_tokens, "SELECT a!b");
+    }
+}
