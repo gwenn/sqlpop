1f7a8957ae4d975810de31b6c2c3ace3271a889f
gwenn <gtreguier@gmail.com>
2016-06-15 22:18:40 +0200

    Add CREATE/DROP TRIGGER parsing.

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -9,9 +9,9 @@ mod test;
 
 pub type ParseError<'input> = lalrpop_util::ParseError<usize, tok::Tok<'input>, tok::Error>;
 
-pub fn parse_sql<'input>(input: &'input str) -> Result<Vec<()>, ParseError<'input>> {
+pub fn parse_sql<'input>(input: &'input str) -> Result<(), ParseError<'input>> {
     let tokenizer = tok::Tokenizer::new(input, 0);
-    let sql = try!(lrsql::parse_CmdList(input, tokenizer));
+    let sql = try!(lrsql::parse_Cmd(input, tokenizer));
 
     Ok(sql)
 }
