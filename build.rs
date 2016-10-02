454cf22cec81b04e881c431e9e2acb346757351e
gwenn <gtreguier@gmail.com>
2016-10-02 12:42:35 +0200

    Try table-driven code-generator

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 extern crate lalrpop;
 
 fn main() {
-    lalrpop::process_root().unwrap();
+    lalrpop::Configuration::new().log_verbose().process_current_dir().unwrap();
+    //lalrpop::process_root().unwrap();
 }
